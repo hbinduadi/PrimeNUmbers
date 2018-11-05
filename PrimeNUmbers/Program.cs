53577a3d8b531a505795e40140449f8b7694d4d3
hbinduadi <42518596+hbinduadi@users.noreply.github.com>
2018-11-05 15:33:11 -0500

    Prime Numbers

--- PrimeNUmbers/Program.cs
+++ PrimeNUmbers/Program.cs
@@ -10,6 +10,28 @@ namespace PrimeNUmbers
     {
         static void Main(string[] args)
         {
+            bool isPrm = true;
+            Console.WriteLine(" The Prime Numbers are : ");
+            for (int counter1 = 2; counter1 <= 100; counter1++)
+            {
+                for (int counter2 = 2; counter2 <= 100; counter2++)
+                {
+                    if ((counter1 != counter2) && (counter1 % counter2 == 0))
+                    {
+                        isPrm = false;
+                        break;
+                    }
+                }
+                if (isPrm)
+                {
+                    Console.WriteLine(counter1);
+                }
+                isPrm = true;
+            }
+            Console.ReadLine();
         }
+        
     }
+        
+    
 }
